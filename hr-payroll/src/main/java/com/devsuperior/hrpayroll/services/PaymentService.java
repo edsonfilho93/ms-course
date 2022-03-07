2666fde8d65dfea03d594d5460719d16ee783a9e
edson <edsonfilho734@gmail.com>
2022-03-07 20:26:16 -0300

    RestTemplate and pom update

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,12 +1,30 @@
 package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> params = new HashMap<>();
+        params.put("id", String.valueOf(workId));
+
+        Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}"), Worker.class, params);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
