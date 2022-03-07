6a101bee76a923ce8c972deba0eda29fae47f6af
edson <edsonfilho734@gmail.com>
2022-03-06 23:12:15 -0300

    hr-payroll implementation

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Payment.java
@@ -0,0 +1,22 @@
+package com.devsuperior.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+
+    public Double getTotal() {
+        return days * dailyIncome;
+    }
+}
